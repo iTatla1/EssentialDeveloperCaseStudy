07b2952286df0cc7c89f3ae152f1a71f963a0710
Muhammad Usman <usm.gh36@gmail.com>
2021-10-16 13:13:54 +0500

    Created `FeedLoader` protocol boundary

---
+++ EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Muhammad Usman Tatla on 10/16/21.
+//
+
+import Foundation
+
+typealias LoadFeedResult  = Result<[FeedItem], Error>
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
