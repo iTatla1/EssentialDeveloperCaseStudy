a2884f4fcb71c05fa18942dd087220e68d3e529a
Muhammad Usman <usm.gh36@gmail.com>
2021-11-12 09:46:23 +0500

    Improved readability with helper method

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -43,7 +43,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         var capturedError = [RemoteFeedLoader.Error]()
         sut.load {capturedError.append($0)}
         let clientError = NSError(domain: "Test", code: 0, userInfo: nil)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedError, [.connectivity])
     }
@@ -66,6 +66,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error,at index: Int = 0) {
+            completions[index](error)
         }
     }
     
