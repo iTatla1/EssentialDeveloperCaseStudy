17c8abefd004c34c084265867b9a38d4d65234ec
Muhammad Usman <usm.gh36@gmail.com>
2021-11-13 21:47:09 +0500

    Delives invalidData error on 200 HTTPResponse with Invalid json data

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -60,6 +60,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJson() {
+        let (sut, client) = makeSUT()
+        
+            var capturedError = [RemoteFeedLoader.Error]()
+            sut.load {capturedError.append($0)}
+        let invalidJSON: Data = Data.init("An invalid JSON".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedError, [.invalidData])
+        
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT (url: URL =  URL(string: "http://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -74,9 +86,9 @@ class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] { messages.map {$0.url} }
         
-        private var messages = [(url: URL, completion: (Result<HTTPURLResponse, Error>) -> Void)]()
+        private var messages = [(url: URL, completion: (Result<(Data,HTTPURLResponse), Error>) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (Result<HTTPURLResponse, Error>) -> Void) {
+        func get(from url: URL, completion: @escaping (Result<(Data,HTTPURLResponse), Error>) -> Void) {
             messages.append((url, completion))
         }
         
@@ -84,11 +96,11 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            
-            messages[index].completion(.success(response))
+            messages[index].completion(.success((data, response)))
         }
+        
     }
     
 }
