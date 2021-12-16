cf1dfe66f3fbbe1cd898aa453c01cdd2fe5aeb2f
Muhammad Usman Tatla <usm.gh36@gmail.com>
2021-12-16 15:48:29 +0500

    Moved FeedStore to its own File

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Muhammad Usman Tatla on 12/16/21.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
